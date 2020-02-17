e5169544644b116b90b7b8a48df0d57a9a369098
Alexander Lund <Alexander.Lund@matrixtelematics.com>
2020-02-17 10:46:40 +0000

    Added Model and Data File

---
+++ DAL/RainData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JBATechTest.DAL
+{
+    class RainData
+    {
+        public int RainDataID { get; set; }
+        public int XRef { get; set; }
+        public int YRef { get; set; }
+        public DateTime Date { get; set; }
+        public int Value { get; set; }
+    }
+}
