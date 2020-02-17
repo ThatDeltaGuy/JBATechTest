dc7db053351dcb64adeb222b08723aaaa3369742
Alexander Lund <Alexander.Lund@matrixtelematics.com>
2020-02-17 10:41:38 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace JBATechTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Console.ReadLine();
+        }
+    }
+}
